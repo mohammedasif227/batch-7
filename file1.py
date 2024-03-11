11fb768f719adeb8783e5a700df7bc6c243b0b11
mohammedasif227 <162983454+mohammedasif227@users.noreply.github.com>
2024-03-11 16:03:08 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,38 @@
+'''
+Day:1
+print("hello world")
+-----> commenting
+1)commenting is used for making document in a python file
+2)when the line is commenting, that line will not be considered
+as a part of the code
+3)when the program is executed, commenting line will not be in the part
+of execution
+----->Note:
+sinle line commenting can be done using # at the begining of the line
+    eg:
+        #print("hello world")
+multiple line commenting can be done using code.capitalize the line
+between the quotes will be commented
+ eg:
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+import this
+
+#import is a keyword which is used to communicated between the python files
+
+import antigravity
+
+----> Bargained characters
+1)control and data structures->ABC language
+2)string and its format->from C
+3)regular expression->pearl
+4)exception handling->module 3
+5)magic methods from->fortorn
+'''
